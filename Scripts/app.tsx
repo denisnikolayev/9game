bfd53782a2c4892d7eb36e98179e8342cbe6795d
Denis Nikolaev <denis_nikolayev@epam.com>
2016-03-19 22:15:23 +0600

    Create

---
+++ Scripts/app.tsx
@@ -0,0 +1,15 @@
+﻿import * as React from "react"
+import {render} from "react-dom";
+
+export class App extends React.Component<{}, {}> {
+    render() {
+        return (
+            <div>
+                <h1 style={{ color: "red" }}>Test</h1>
+               
+            </div>
+        );
+    }
+}
+
+render(<App />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+﻿// ReSharper disable once PossiblyUnassignedProperty
+var path = require("path");
+
+module.exports = {
+    devtool: "",
+    entry: [
+         "webpack-dev-server/client?http://localhost:3002",
+         "webpack/hot/only-dev-server",
+        "./scripts/app.tsx"
+    ],
+    output: {
+        path: path.join(__dirname, "wwwroot"),
+        filename: "bundle.js",
+        publicPath: "/"
+    },
+    resolve: {
+        extensions: [".tsx", ".js", ""]
+    },
+    module: {
+        loaders: [{
+            test: /\.tsx?$/,
+            loaders: ["react-hot", "ts-loader"],
+            include: path.join(__dirname, "scripts")
+        }]
+    },
+    debug: false,
+    devServer: {
+        contentBase: "./wwwroot",
+        host: "localhost",
+        port: 3002,
+        proxy: {
+            '/api/*': {
+                target: 'http://localhost:30155',
+                secure: false
+            }
+        }
+    }
+};
