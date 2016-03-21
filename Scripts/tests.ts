733768b781f0bef7b9de8ffaedb9dbb619b9b020
Denis Nikolaev <denis_nikolayev@epam.com>
2016-03-21 18:29:54 +0600

    Lost file

---
+++ Scripts/tests.ts
@@ -0,0 +1,12 @@
+﻿import * as tsUnit from "./external/tsUnit";
+import {ImagePositionFinderTests} from "./utils/imagePositionFinder.test";
+import {GameTests} from "./model/game.test";
+
+var tests = [
+    ImagePositionFinderTests,
+    GameTests
+];
+
+var tap = new tsUnit.Test(tests).run().getTapResults();
+
+console.log(tap);
