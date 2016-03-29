cb8b117e34c5dc5538da874cd2e9bfdb386261f5
Denis Nikolaev <denis_nikolayev@epam.com>
2016-03-29 18:38:02 +0600

    try to fix working in ie

--- Client/app.tsx
+++ Client/app.tsx
@@ -1,4 +1,7 @@
-﻿import * as React from "react"
+﻿
+import "babel-polyfill";
+
+import * as React from "react"
 import {render} from "react-dom";
 
 import {GamePage} from "./pages/gamePage";
--- Client/webpack.config.js
+++ Client/webpack.config.js
@@ -5,16 +5,18 @@ var webpack = require("webpack");
 var WebpackNotifierPlugin = require('webpack-notifier');
 
 module.exports = {
-    devtool: "inline-source-map",
-    entry: process.env.NODE_ENV == "development"?[
-         "webpack-dev-server/client?http://localhost:3002",
-         "webpack/hot/only-dev-server",
-         "./sass/app.scss",
-         "./app.tsx"
-    ] : [
-        "./sass/app.scss",
-         "./app.tsx"
-    ],
+    devtool: "", //process.env.NODE_ENV == "development"?"inline-source-map":"",
+    entry: process.env.NODE_ENV == "development"
+        ? [
+            "webpack-dev-server/client?http://localhost:3002",
+            "webpack/hot/only-dev-server",
+            "./sass/app.scss",
+            "./app.tsx"
+        ]
+        : [
+            "./sass/app.scss",
+            "./app.tsx"
+        ],
     output: {
         path: path.join(__dirname, "../Server/wwwroot"),
         filename: "bundle.js",
@@ -24,9 +26,10 @@ module.exports = {
         extensions: [".tsx", ".js", "", ".ts", '.scss', '.png', '.jpg']
     },
     module: {
-        loaders: [{
-            test: /\.tsx?$/,
-            loaders: ["react-hot", "ts-loader"],
+        loaders: [
+        {
+            test: /(\.tsx|\.ts)$/,
+            loaders: process.env.NODE_ENV === "development" ? ["react-hot", "babel-loader?presets[]=es2015&plugins[]=transform-runtime&presets[]=stage-0", "ts-loader"] : ["babel-loader?presets[]=es2015&plugins[]=transform-runtime&presets[]=stage-0", "ts-loader"],
             include: __dirname
         },
         { test: /\.png|\.jpg$/, loader: "url-loader?limit=100000" },
@@ -48,12 +51,15 @@ module.exports = {
         }),
         new WebpackNotifierPlugin()
     ],
-    debug: false,
+    debug: true,
     devServer: {
         contentBase: "./wwwroot",
         host: "localhost",
         port: 3002,
         proxy: {
+            '/': {
+                target: 'http://localhost:30155'
+            },
             '/api/*': {
                 target: 'http://localhost:30155'
             },
